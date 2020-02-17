5821ab2ed9355bcd9184070a27f99f022ea21b1f
Tyler Nijmeh <tylernij@gmail.com>
2020-02-16 22:56:44 -0800

    sched: Process new forks before processing their parent
    This should let brand new tasks launch marginally faster.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -104,7 +104,7 @@ static unsigned int normalized_sysctl_sched_base_slice	= 700000ULL;
  * After fork, child runs first. If set to 0 (default) then
  * parent will (try to) run first.
  */
-unsigned int sysctl_sched_child_runs_first __read_mostly;
+unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 
 const_debug unsigned int sysctl_sched_migration_cost	= 0UL;
 
