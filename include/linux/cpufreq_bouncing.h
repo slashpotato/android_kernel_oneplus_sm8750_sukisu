3f62b77bb76eb720b6509af304fe4f0d57111613
brokestar233 <3765589194@qq.com>
2025-11-07 05:36:51 +0800

    drivers: cpufreq_bouncing: add cpufreq_bouncing
    Change-Id: Id0ae587ee3fa6e46602f8f995cad28f2286b33ac
    Signed-off-by: brokestar233 <3765589194@qq.com>

---
+++ include/linux/cpufreq_bouncing.h
@@ -0,0 +1,43 @@
+#ifndef __CPUFREQ_BOUNCING_H__
+#define __CPUFREQ_BOUNCING_H__
+
+#include <linux/cpufreq.h>
+
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_GKI_CPUFREQ_BOUNCING)
+void cb_update(struct cpufreq_policy *pol, u64 time);
+void cb_reset(int cpu, u64 time);
+unsigned int cb_cap(struct cpufreq_policy *pol, unsigned int freq);
+void cb_stuff_init(struct cpufreq_policy *policy);
+void cb_ceiling_free_enable(bool rescue_enable);
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_CEILING_FREE)
+void cb_ceiling_free(bool ceiling_free_enable);
+#endif
+#else
+static inline void cb_update(struct cpufreq_policy *pol, u64 time)
+{
+}
+
+static inline void cb_reset(int cpu, u64 time)
+{
+}
+
+static inline unsigned int cb_cap(struct cpufreq_policy *pol, unsigned int freq)
+{
+}
+
+static inline void cb_stuff_init(struct cpufreq_policy *policy)
+{
+}
+static inline void cb_ceiling_free_enable(bool rescue_enable)
+{
+}
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_CEILING_FREE)
+static inline void cb_ceiling_free(bool rescue_enable)
+{
+}
+#endif
+
+#endif
+
+#endif /* __CPUFREQ_BOUNCING_H__ */
+
