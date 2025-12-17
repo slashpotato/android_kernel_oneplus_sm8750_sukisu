46284d6d9617dff4e9680fa40d0b104a9793b531
brokestar233 <3765589194@qq.com>
2025-12-17 22:25:13 +0800

    zram: embed writeback request in bio via bioset front_pad
    To reduce memory allocations in the zram writeback path, embed the
    zram_wb_request structure directly in the bio's front pad.
    
    Initialize a dedicated bio_set with front_pad set to the size of
    struct zram_wb_request. Use bio_alloc_bioset() to allocate bios from
    this set, allowing direct access to the embedded request via bio_data().
    
    This eliminates the separate kmalloc/kfree for each request and the
    bi_private pointer assignment.
    
    Also add proper bioset_exit() in cleanup paths.
    
    Change-Id: I6f8c50eef53fb65498786557af366b0bfbd4af8d
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- drivers/block/zram/zram_wb.c
+++ drivers/block/zram/zram_wb.c
@@ -8,12 +8,28 @@
 #include <linux/kthread.h>
 #include <linux/wait.h>
 #include <linux/freezer.h>
+#include <linux/blkdev.h>
 
 #include "zram_wb.h"
 
 static struct task_struct *wb_thread;
 static DECLARE_WAIT_QUEUE_HEAD(wb_wq);
 static struct zram_wb_request_list wb_req_list;
+static struct bio_set zram_wb_bs;
+
+/* 
+ * front_pad: 在 bio 结构之前预留空间存放 zram_wb_request
+ * 需要对齐以保证 bio 结构的正确对齐
+ */
+#define ZRAM_WB_FRONT_PAD \
+	roundup(sizeof(struct zram_wb_request), __alignof__(struct bio))
+
+/*
+ * 从 bio 指针获取其前面的 zram_wb_request 结构
+ * bio 分配时在其前面预留了 ZRAM_WB_FRONT_PAD 字节
+ */
+#define bio_to_zram_wb_request(bio) \
+	((struct zram_wb_request *)((char *)(bio) - ZRAM_WB_FRONT_PAD))
 
 unsigned long alloc_block_bdev(struct zram *zram)
 {
@@ -165,8 +181,7 @@ static int wb_thread_func(void *data)
 
 static void zram_writeback_end_io(struct bio *bio)
 {
-	struct zram_wb_request *req =
-		(struct zram_wb_request *)bio->bi_private;
+	struct zram_wb_request *req = bio_to_zram_wb_request(bio);
 
 	enqueue_wb_request(&wb_req_list, req);
 	wake_up(&wb_wq);
@@ -186,17 +201,19 @@ struct zram_wb_request *alloc_wb_request(struct zram *zram,
 	if (!page)
 		return ERR_PTR(-ENOMEM);
 
-	bio = bio_alloc(zram->bdev, 1, REQ_OP_WRITE, GFP_NOIO | __GFP_NOWARN);
+	/*
+	 * 使用 bioset 分配 bio,front_pad 空间会被自动分配在 bio 之前
+	 * 需要 BIOSET_NEED_BVECS 标志,因为我们手动添加 page
+	 */
+	bio = bio_alloc_bioset(zram->bdev, 1, REQ_OP_WRITE, GFP_NOIO,
+			       &zram_wb_bs);
 	if (!bio) {
 		err = -ENOMEM;
 		goto out_free_page;
 	}
 
-	req = kmalloc(sizeof(struct zram_wb_request), GFP_NOIO | __GFP_NOWARN);
-	if (!req) {
-		err = -ENOMEM;
-		goto out_free_bio;
-	}
+	/* 通过宏访问 bio 前面的 zram_wb_request 结构 */
+	req = bio_to_zram_wb_request(bio);
 	req->zram = zram;
 	req->pps = pps;
 	req->ppctl = ppctl;
@@ -205,12 +222,9 @@ struct zram_wb_request *alloc_wb_request(struct zram *zram,
 
 	bio->bi_iter.bi_sector = blk_idx * (PAGE_SIZE >> 9);
 	__bio_add_page(bio, page, PAGE_SIZE, 0);
-	bio->bi_private = req;
 	bio->bi_end_io = zram_writeback_end_io;
 	return req;
 
-out_free_bio:
-	bio_put(bio);
 out_free_page:
 	__free_page(page);
 	return ERR_PTR(err);
@@ -221,13 +235,33 @@ void free_wb_request(struct zram_wb_request *req)
 	struct bio *bio = req->bio;
 	struct page *page = bio_first_page_all(bio);
 
-	__free_page(page);
+	if (page)
+		__free_page(page);
+	/*
+	 * bio_put 会自动释放 bio 以及其 front_pad 空间
+	 * 不需要单独释放 req
+	 */
 	bio_put(bio);
-	kfree(req);
 }
 
 int setup_zram_writeback(void)
 {
+	/*
+	 * 初始化 bioset:
+	 * - pool_size: 64,预分配的 bio 数量,用于减少分配开销
+	 * - front_pad: ZRAM_WB_FRONT_PAD,在每个 bio 之前预留空间
+	 * - flags: BIOSET_NEED_BVECS,需要 bio vecs 支持
+	 * 
+	 * 参考 dm-table.c 的做法,使用 front_pad 可以:
+	 * 1. 避免为 zram_wb_request 单独分配内存
+	 * 2. 提高缓存局部性(request 和 bio 内存连续)
+	 * 3. 简化内存管理(一起分配一起释放)
+	 */
+	if (bioset_init(&zram_wb_bs, 64, ZRAM_WB_FRONT_PAD, BIOSET_NEED_BVECS)) {
+		pr_err("Unable to init zram_wb_bs\n");
+		return -1;
+	}
+
 	spin_lock_init(&wb_req_list.lock);
 	INIT_LIST_HEAD(&wb_req_list.head);
 	wb_req_list.count = 0;
@@ -235,6 +269,7 @@ int setup_zram_writeback(void)
 	wb_thread = kthread_run(wb_thread_func, NULL, "zram_wb_thread");
 	if (IS_ERR(wb_thread)) {
 		pr_err("Unable to create zram_wb_thread\n");
+		bioset_exit(&zram_wb_bs); 
 		return -1;
 	}
 	return 0;
@@ -244,5 +279,5 @@ void destroy_zram_writeback(void)
 {
 	kthread_stop(wb_thread);
 	destroy_wb_request_list(&wb_req_list);
+	bioset_exit(&zram_wb_bs);
 }
-
